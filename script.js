bdcc2bbcf1d0d75d7a38418702fa8a28646a4297
thiago passeri <119812332+thiagopasseri@users.noreply.github.com>
2024-11-23 01:40:55 +0100

    add clear function and inputing a number after = to force a clear()

--- script.js
+++ script.js
@@ -2,21 +2,24 @@
 const display = document.querySelector(".display");
 const input = document.querySelector(".inputs");
 const log = document.querySelector(".log");
+
 //global variables
-let gPreviousOperand = '';
-let gCurrentOperand = '';
+let gPreviousOperand = '0';
+let gCurrentOperand = '0';
 let gResultNumber = 0;
 let gLastOp = '';
 
 function displayLog(){
-    log.textContent=`previous = ${gPreviousOperand}      `;
-    log.textContent +=`result = ${gResultNumber}`;
+    log.textContent =`result=${gResultNumber}     ` ;
+    log.textContent +=`lastOp=${gLastOp}`;
+
 }
 
 function refreshOperand(inputNumber){
     if ((gCurrentOperand).length === 9) return;
     // gCurrentOperand += inputNumber;
-    if(gCurrentOperand === ''){
+    if(gLastOp =="_=") clear();
+    if(gCurrentOperand === '0'){
         gCurrentOperand = inputNumber;
         display.textContent = gCurrentOperand;
         gResultNumber = calculate(gLastOp);
@@ -26,52 +29,64 @@ function refreshOperand(inputNumber){
     } else {
         gCurrentOperand += inputNumber;
         display.textContent = gCurrentOperand;
-        gResultNumber = calculate();
+        console.log(gLastOp);
+
+        gResultNumber = calculate(gLastOp);
 
     }
 }
 
-
-function clearDisplay() {
-    display.textContent = '';
-}
-
-function updateResult(input) {
-    gResultNumber = calculate(input);
-
+function clear() {
+    gLastOp='';
+    gPreviousOperand = '0';
+    gResultNumber = '';
+    gCurrentOperand = '0';
+    display.textContent = gPreviousOperand;
 }
 
 function calculate(inputId) {
+    // if that checks if a operator is being pressed after the other, in this case
+    // the display value becames the currentOperand
+    if (gCurrentOperand === '') gCurrentOperand = display.textContent;
     switch(inputId){
         case "_+":
             return +gPreviousOperand + +gCurrentOperand;
         case "_/":
             return +gPreviousOperand / +gCurrentOperand;
         case "_*":
+            console.log("entrou produto")
             return +gPreviousOperand * +gCurrentOperand;
         case "_-":
             return +gPreviousOperand - +gCurrentOperand;
         case "_=":
-            return +gResultNumber;
+            console.log("entrou =")
+            return +gResultNumber;  
+        case "":
+            console.log("entrou vazio")
+            return 0;
+        default:
+            console.log("entrou default")
+            return 0;
 }}
 
 function userClick(event) {
     let clickedElement = event.target;
+    if(clickedElement.id === "clear") clear();
     if (clickedElement.classList[0] === "number") {
         refreshOperand(clickedElement.textContent);
     }
     if (clickedElement.classList[0] === "operator") {
-        if (gPreviousOperand === ''){
+        if (gPreviousOperand === '0'){
             gPreviousOperand = gCurrentOperand;
             gCurrentOperand = '';
-            lastOp = clickedElement.id;
+            gLastOp = clickedElement.id;
 
         }
         else {
-            gResultNumber = calculate(lastOp);
+            gResultNumber = +String(calculate(gLastOp)).slice(0,9);
             display.textContent = String(gResultNumber);
             gPreviousOperand = String(gResultNumber);
-            lastOp = clickedElement.id;
+            gLastOp = clickedElement.id;
             gCurrentOperand = '';
         }
         displayLog();
@@ -79,15 +94,4 @@ function userClick(event) {
         }
     }
 
-    // switch(event.target.id) {
-    //     case _0:
-    //         // code
-    //         break;
-    //     case 1:
-    //         // code
-    //         break;
-    //     case 1:
-    //         // code
-    //         break;      
-  
 input.addEventListener("click", userClick);
