22752c19318e1e4d9d95142e199a8ce29a9fedec
thiago passeri <119812332+thiagopasseri@users.noreply.github.com>
2024-11-23 13:07:12 +0100

    update UI

--- script.js
+++ script.js
@@ -17,19 +17,20 @@ function displayLog(){
 }
 function refreshOperand(inputNumber){
     if ((gCurrentOperand).length === 9) return;
-    // gCurrentOperand += inputNumber;
+    // serves to clear the display if the user is inputing a number after a
+    // final result
     if(gLastOp =="_=") clear();
     if(gCurrentOperand === '0'){
         gCurrentOperand = inputNumber;
         display.textContent = gCurrentOperand;
         gResultNumber = calculate(gLastOp);
-        displayLog();
+        
 
     } else {
         gCurrentOperand += inputNumber;
         display.textContent = gCurrentOperand;
         gResultNumber = calculate(gLastOp);
-        displayLog();
+        
 
     }
 }
@@ -40,7 +41,7 @@ function clear() {
     gResultNumber = '';
     gCurrentOperand = '0';
     display.textContent = gPreviousOperand;
-    displayLog();
+    
 
 }
 
@@ -54,18 +55,14 @@ function calculate(inputId) {
         case "_/":
             return +gPreviousOperand / +gCurrentOperand;
         case "_*":
-            console.log("entrou produto")
             return +gPreviousOperand * +gCurrentOperand;
         case "_-":
             return +gPreviousOperand - +gCurrentOperand;
         case "_=":
-            console.log("entrou =")
             return +gResultNumber;  
         case "":
-            console.log("entrou vazio")
             return 0;
         default:
-            console.log("entrou default")
             return 0;
 }}
 
@@ -76,20 +73,21 @@ function userClick(event) {
             clear();
             return;
         case  "_.":
+            // serves to refresh the lastOp (_=) when . is pressed after a final result
             if(gLastOp =="_=") clear();
             if (gCurrentOperand.includes(".")) return;
             else {
                 if(gCurrentOperand === "0") {
                     gCurrentOperand = "0.";
                     display.textContent = gCurrentOperand;
-                    displayLog();
+                    
 
                 }
                 else {
                     gCurrentOperand += ".";
                     console.log(gCurrentOperand);
                     display.textContent = gCurrentOperand;
-                    displayLog();
+                    
                 }
             }
             return;
@@ -112,7 +110,7 @@ function userClick(event) {
                 gLastOp = clickedElement.id;
                 gCurrentOperand = '0';
             }
-            displayLog();
+            
 }}
 
 input.addEventListener("click", userClick);
